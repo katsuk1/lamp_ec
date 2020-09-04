6d44c3a7116544e4344e6ab19ad9ed0c6346c239
[satoukatsuki] <[katsuki0607@gmail.com]>
2020-09-04 17:24:04 +0900

    レスポンシブ対応 #3

--- view/cart_view.php
+++ view/cart_view.php
@@ -19,17 +19,16 @@
                 </div>
                 <div>
 <?php include VIEW_PATH . 'templates/messages.php'; ?>
-
+                    <div class="item-wrapper">
 <?php foreach ($carts as $cart) { ?>
-                    <div class="item">
-                        <div>
+                        <div class="item">
                             <p><?php print h($cart['name']); ?></p>
                             <div class = "img">
                                 <img src = "<?php print h(IMAGE_PATH . $cart['img']); ?>">
                             </div>
                             <p>価格:<?php print h($cart['price']); ?>円(税込)</p>
                             <form method="post" action="cart_change_amount.php">
-                                <input type="number"   name="amount" value="<?php print h($cart['amount']); ?>">個&nbsp;&nbsp;
+                                <input type="number" class="number" name="amount" value="<?php print h($cart['amount']); ?>">個&nbsp;&nbsp;
                                 <input type="hidden" name="cart_id" value="<?php print h($cart['cart_id']); ?>">
                                 <input type="hidden" name = "csrf_token" value="<?php print $token;?>">
                                 <input type="submit" value="数量を変更" class="btn btn-primary">
@@ -40,8 +39,8 @@
                                 <input type="submit" value="削除" class = "delete">
                             </form>
                         </div>
-                    </div>
 <?php } ?>
+                    </div>
                     <div class="buy-sum-box">
                         <p>合計:<?php print h(number_format($total_price)); ?>円(税込)</p>
                     </div>
